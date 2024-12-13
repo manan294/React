0bbebfe1ce0459a6eaaa2e5e324f0680c82c5afb
manan294 <mananp294@gmail.com>
2024-12-13 13:05:17 +0530

    sidebar&navbar

---
+++ src/components/sidebar&navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className="blank">
+      <div className="container">
+        <h1>LOGO</h1>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
